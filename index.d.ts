07d6184bd699386a370b98a5dedc8c9c40ca75e6
Carrie Ho <carrie503.ch@gmail.com>
2018-07-09 12:21:20 +0800

    Fix index.d.ts
    We are waiting for PR https://github.com/react-native-community/react-native-linear-gradient/pull/280/files merged
    Then we can remove this branch

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,14 @@ import * as ReactNative from "react-native";
 
 declare module "react-native-linear-gradient" {
 
-    export interface LinearGradientProps extends ReactNative.ViewProperties {
+    export interface LinearGradientProps {
         colors: (string|number)[],
         start?: { x: number, y: number },
         end?: { x: number, y: number },
         locations?: number[]
     }
 
-    export class LinearGradient extends React.Component<LinearGradientProps, any> { }
+    type Props = LinearGradientProps & ReactNative.ViewProperties;
 
-    export default LinearGradient
+    export default class LinearGradient extends React.Component<Props, any> { }
 }
